ef34fd579ed9c49265cf25e7c8ebc195c241d455
dyerduman <hello@dyerdumandesign.ca>
2021-03-05 19:18:44 -0500

    Test1

---
+++ README.md
@@ -0,0 +1,4 @@
+# goodwillamity_2021
+Theme design for Goodwill Amity 2021, WCAG 2.0 Compliant
+
+A test update here to ensure Atom is talking to Git
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'U6wazDMc6DiTgXcHET2xJ4qwzcrY0cVwE3FYqoNRiG0zMGJg2xL5G7vUu6wbTCrD');
+define('SECURE_AUTH_KEY',  'qjquHXwzWAEdV1fLzh7r4NJcJgeeW9yvVMB6tDpJKoH610D5jyCzDBNCjBQHas90');
+define('LOGGED_IN_KEY',    'deS7n6cpuj29E3rxfxqC31MBhoiny96RefSPaiRdrCP9PqGyoXbIDem5do6rKN3u');
+define('NONCE_KEY',        'pGE6y2pnYuDBN2HRHKH2JnaCE1IQBs1XmdicEdNaaDBR4IAohw5I3QGytaGR5qj2');
+define('AUTH_SALT',        '61VQYoo8FdiTFQ3rKRFuRHPRoA5hfESSMK8pcwnpxpG50yJWdipvzpgh9Gm1SqxR');
+define('SECURE_AUTH_SALT', 'DmJyARNz2hDYsivaszG1PjI1dNtRTDBnYTABbMjJ4yLyE9bGmDr35mrq8vPoIdzR');
+define('LOGGED_IN_SALT',   'VN2qYqd4Xs850wMeSyCYPa8uNy8YgbuShApzQmJTqYsx1pMT7IRsLni1Hvs07gMP');
+define('NONCE_SALT',       '81ICND3Tbh1xp6YN2yYq4x4odSasNQKFQwKwhXd2LQU91YmH2uuTp942y6LVRu4n');
